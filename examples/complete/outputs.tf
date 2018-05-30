ce2bb7870e99a03bc0f3df58211608c9078257fb
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-05-30 12:34:02 +0700

    fix typo

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -3,21 +3,21 @@ output "timeboard_title" {
 }
 
 output "monitor_server_latency_p95_name" {
-  value = "${module.rpc.server_latency_p95_name}"
+  value = "${module.rpc.monitor_server_latency_p95_name}"
 }
 
 output "monitor_server_exception_name" {
-  value = "${module.rpc.server_exception_name}"
+  value = "${module.rpc.monitor_server_exception_name}"
 }
 
 output "monitor_client_latency_p95_name" {
-  value = "${module.rpc.client_latency_p95_name}"
+  value = "${module.rpc.monitor_client_latency_p95_name}"
 }
 
 output "monitor_client_exception_name" {
-  value = "${module.rpc.client_exception_name}"
+  value = "${module.rpc.monitor_client_exception_name}"
 }
 
 output "monitor_circuit_breaker_status_name" {
-  value = "${module.rpc.circuit_breaker_status_name}"
+  value = "${module.rpc.monitor_circuit_breaker_status_name}"
 }
